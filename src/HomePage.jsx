95a4c68360b0b65e180b617824ac603ea27fa9a4
IacobOliver <olialexander08@gmail.com>
2024-01-11 14:14:02 +0200

    init project

---
+++ src/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function HomePage() {
+ 
+
+  return (
+   
+     <div>
+      hello world
+     </div>
+    
+   
+  )
+}
+
+export default HomePage
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './HomePage';
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <div>Hello</div>
+      <Routes>
+        <Route path="/home" element={<HomePage/>}> </Route>
+       
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

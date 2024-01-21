fa27abe6411ab79b4f823a350a217758d2c22a5d
IacobOliver <olialexander08@gmail.com>
2024-01-21 15:51:01 +0200

    almost done hero presentation

--- src/components/Button1.jsx
+++ src/components/Button1.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Button } from "@material-tailwind/react";
 
-export default function Button1({text, event}){
+export default function Button1({text, event, className}){
     return(
-        <Button variant="text" className="rounded-lg mx-1 text-lg capitalize">
+        <Button variant="text" className={`rounded-lg mx-1 text-lg capitalize ${className}`}>
          {text}
       </Button>
     )

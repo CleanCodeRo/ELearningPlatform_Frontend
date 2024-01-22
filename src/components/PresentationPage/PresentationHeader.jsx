20eab87af09cf2873cf4314d6412fca2b176c01a
Ionut2707 <ionut.dima2707@gmail.com>
2024-01-23 01:48:35 +0200

    login nu merge

--- src/components/PresentationPage/PresentationHeader.jsx
+++ src/components/PresentationPage/PresentationHeader.jsx
@@ -1,14 +1,56 @@
-import { Popover, PopoverHandler, Button } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const PresentationHeader = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login(event) {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      await fetch("http://localhost:8080/users/auth/authenticate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Http error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Success: ", data);
+          navigate("/home");
+        });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return (
     <div id="PresentationHeader" className="bg-gray-250 px-[6rem] font-inter">
       <div className="flex justify-between items-center">
-        <img draggable={false} className="w-28" src="/images/CleanCode-removebg-preview.png" />
+        <img
+          draggable={false}
+          className="w-28"
+          src="/images/CleanCode-removebg-preview.png"
+        />
         <div className="flex space-x-1.5">
           <div className="w-64 ">
             <div className="relative h-10 min-w-[200px] ">
               <input
+                onChange={(e) => setUsername(e.target.value)}
                 type="text"
                 placeholder="Username"
                 className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
@@ -18,17 +60,21 @@ const PresentationHeader = () => {
           <div className="w-64">
             <div className="relative h-10 min-w-[200px] ">
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 type="password"
                 placeholder="Password"
                 className="peer h-full w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50"
               />
             </div>
           </div>
-          <Popover>
-            <PopoverHandler>
-              <Button id="loginHeader" className="bg-fourth">Login</Button>
-            </PopoverHandler>
-          </Popover>
+
+          <Button
+            id="loginButton"
+            className="bg-fourth font-inter shadow-2xl"
+            onClick={(event) => login(event)}
+          >
+            Login
+          </Button>
         </div>
       </div>
     </div>

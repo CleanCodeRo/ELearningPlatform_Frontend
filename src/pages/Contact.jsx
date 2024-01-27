fbdb5d21fea82c402066ac0955786057c0681441
Ionut2707 <ionut.dima2707@gmail.com>
2024-01-27 18:07:46 +0200

    modify contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
           <div className="mb-5">
             <label
               for="name"
-              className="mb-3 block text-base font-medium text-[#07074D]"
+              className="mb-3 block text-base font-medium text-fifth"
             >
               Full Name
             </label>
@@ -15,13 +15,13 @@ const Contact = () => {
               name="name"
               id="name"
               placeholder="Full Name"
-              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border border-[#e0e0e0] bg-gray-300 py-3 px-6 text-base font-medium text-fourth outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
           <div className="mb-5">
             <label
               for="email"
-              className="mb-3 block text-base font-medium text-[#07074D]"
+              className="mb-3 block text-base font-medium text-fifth"
             >
               Email Address
             </label>
@@ -30,13 +30,13 @@ const Contact = () => {
               name="email"
               id="email"
               placeholder="example@domain.com"
-              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border border-[#e0e0e0] bg-gray-300 py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
           <div className="mb-5">
             <label
               for="subject"
-              className="mb-3 block text-base font-medium text-[#07074D]"
+              className="mb-3 block text-base font-medium text-fifth"
             >
               Subject
             </label>
@@ -45,13 +45,13 @@ const Contact = () => {
               name="subject"
               id="subject"
               placeholder="Enter your subject"
-              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full rounded-md border border-[#e0e0e0] bg-gray-300 py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             />
           </div>
           <div className="mb-5">
             <label
               for="message"
-              className="mb-3 block text-base font-medium text-[#07074D]"
+              className="mb-3 block text-base font-medium text-fifth"
             >
               Message
             </label>
@@ -60,11 +60,11 @@ const Contact = () => {
               name="message"
               id="message"
               placeholder="Type your message"
-              className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+              className="w-full resize-none rounded-md border border-[#e0e0e0] bg-gray-300 py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
             ></textarea>
           </div>
           <div>
-            <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none">
+            <button className=" hover:shadow-form rounded-md bg-fifth py-3 px-8 text-base font-semibold text-white outline-none">
               Submit
             </button>
           </div>

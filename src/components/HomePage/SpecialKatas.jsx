82659fd9d576cdebe247ee1e33a3bdce9f1d2a0a
bogdandiaconupv <bogdan.diaconupv@gmail.com>
2024-02-04 01:20:16 +0200

    kata ideea need np, i

--- src/components/HomePage/SpecialKatas.jsx
+++ src/components/HomePage/SpecialKatas.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import KataCard from "../KataCard";
+import Carousel from "../Carousel";
 
 export default function SpecialKatas(){
 
@@ -16,9 +18,14 @@ export default function SpecialKatas(){
         <div id="conatainer" className=" w-full   pt-5 pb-10">
           <p id="title"  className="text-4xl p-4 text-fourth  font-bold">Special Katas</p>
          
-         <div id="filterButtons">
-            
-         </div>
+          <div id="containerSlider" className="bg-black flex justify-center items-center " style={{ width: 'calc(100% - 80px)' }}>
+
+    <Carousel />
+  
+</div>
+         
+         
+         
         </div>
     )
 }

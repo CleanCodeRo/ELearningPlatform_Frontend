02f68b49d490858848b4b0e68e2400dc2029de37
IacobOliver <olialexander08@gmail.com>
2024-02-14 14:01:48 +0200

    status almost done for lessons

--- src/components/ShowModule/LessonCard.jsx
+++ src/components/ShowModule/LessonCard.jsx
@@ -4,17 +4,20 @@ import {
   MenuItem,
   MenuList,
 } from "@material-tailwind/react";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Select, Option } from "@material-tailwind/react";
+import { useAtom } from "jotai";
+import state from "../Atom";
 
 export default function LessonCard({ id, name, description, gitHubLink, userRole }) {
+  const [user, setUser] = useAtom(state.user);
   const lessonCard = useRef(null);
   const navigate = useNavigate();
   const params = useParams();
 
   const deleteLesson = async (e) => {
     e.stopPropagation();
-
     try {
       await fetch(`http://localhost:8080/lessons?lessonId=${id}`, {
         method: "DELETE",
@@ -33,32 +36,78 @@ export default function LessonCard({ id, name, description, gitHubLink, userRole
   const EditPen = () => {
     return (
       userRole == "ADMIN" ?
-      <Menu>
-        <MenuHandler>
-          <i className="fa-solid fa-pen p-2 text-first bg-[rgba(255,255,255,0.7)] hover:bg-[rgba(255,255,255,1)] rounded-xl duration-300 cursor-pointer mx-3"></i>
-        </MenuHandler>
-        <MenuList className=" bg-first bg-opacity-40 backdrop-blur-md border-0 text-sixth ">
-          <MenuItem
-            onClick={(e) => {  deleteLesson(e)}}
-            className="bg-first bg-opacity-80 mb-1"
-          >
-            <i className="fa-solid fa-trash-can mr-1" /> Delete
-          </MenuItem>
-          <MenuItem
-            onClick={(e) => {
-              e.stopPropagation();
-              navigate(
-                `/home/module/${params.moduleId}/week/${params.weekId}/editLesson/${id}`
-              );
-            }}
-            className="bg-first bg-opacity-80"
-          >
-            <i className="fa-solid fa-pen-to-square mr-1" /> Edit
-          </MenuItem>
-        </MenuList>
-      </Menu> : null 
+        <Menu>
+          <MenuHandler>
+            <i className="fa-solid fa-pen p-2 text-first bg-[rgba(255,255,255,0.7)] hover:bg-[rgba(255,255,255,1)] rounded-xl duration-300 cursor-pointer mx-3"></i>
+          </MenuHandler>
+          <MenuList className=" bg-first bg-opacity-40 backdrop-blur-md border-0 text-sixth ">
+            <MenuItem
+              onClick={(e) => { deleteLesson(e) }}
+              className="bg-first bg-opacity-80 mb-1"
+            >
+              <i className="fa-solid fa-trash-can mr-1" /> Delete
+            </MenuItem>
+            <MenuItem
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate(
+                  `/home/module/${params.moduleId}/week/${params.weekId}/editLesson/${id}`
+                );
+              }}
+              className="bg-first bg-opacity-80"
+            >
+              <i className="fa-solid fa-pen-to-square mr-1" /> Edit
+            </MenuItem>
+          </MenuList>
+        </Menu> : null
     );
   };
+
+  const EditStatusComponent = () => {
+    const [value, setValue] = useState(user.completedLessons.includes(id) ? "DONE" : "TODO");
+    return (
+      <div id="holder" className="">
+        <Select
+          size="xl"
+          color="blue"
+          id="select"
+          label="Select Status"
+          value={value}
+          onChange={(status) => EditStatusEvent(status)}
+          className="text-sixth  "
+        >
+          <Option  value="DONE">Done</Option>
+          <Option  value="TODO">To Do</Option>
+
+        </Select>
+      </div>
+    );
+  };
+
+  const EditStatusEvent = (status) =>{
+    if(status == "DONE"){
+      user.completedLessons.push(id)
+    }else{
+      user.completedLessons =  user.completedLessons.filter(item => item != id);
+    }
+
+    fetch(` http://localhost:8080/users/${user.id}/addOrRemoveLesson/${id}`,{
+      method : "PATCH",
+      headers :{
+        "Content-Type" : "application/json",
+        Authorization : `Bearer ${localStorage.getItem("ELearningToken")}`
+      },
+      body : JSON.stringify(status)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setValue(status);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
 
   return (
     <div
@@ -91,9 +140,7 @@ export default function LessonCard({ id, name, description, gitHubLink, userRole
         >
           Learn
         </a>
-        <div id="status" className="py-2 px-3 rounded-xl bg-orange-800">
-          DONE
-        </div>
+        < EditStatusComponent />
       </div>
     </div>
   );

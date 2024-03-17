ff22b9e09223135a82d2b740f56189312af80eaa
bogdandiaconupv <bogdandiaconupv@gmail.com>
2024-03-17 23:58:50 +0200

    loading+ full delete front+back

--- src/components/HomePage/SpecialKatas/NewKataCard.jsx
+++ src/components/HomePage/SpecialKatas/NewKataCard.jsx
@@ -6,9 +6,10 @@ import {
   MenuItem,
   MenuList,
 } from "@material-tailwind/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useAtom } from "jotai";
-import state from "../../Atom";
+import state, { getCompletedStuff } from "../../Atom";
+import Loading from "../../Loading/Loading";
 
 export default function NewKataCard({ kata }) {
   const kataCardRef = useRef(null);
@@ -17,37 +18,42 @@ export default function NewKataCard({ kata }) {
   const [completedModules, setCompletedModules] = useAtom(state.completedModules);
   const [completedKatas, setCompletedKatas] = useAtom(state.completedKatas)
   const [user, setUser] = useAtom(state.user)
+  const [isCompleted, setIsCompleted] = useState(completedKatas.includes(kata.id) ? true : false);
+  const [loading, setLoading] = useState(false);
 
-  let isCompleted = completedKatas.includes(kata.id) ? true :false;
   // let isCompleted = true;
-  
- 
 
-  
-  const completeKataEvent = (E) => {
-    fetch(` http://localhost:8080/users/addCompleteKata?userId=${user.id}&kataId=${kata.id}`,{
-      method : "PATCH",
-      headers :{
-        "Content-Type" : "application/json",
-        Authorization : `Bearer ${localStorage.getItem("ELearningToken")}`
+
+
+
+  const completeKataEvent = (e) => {
+    e.target.disabled = true;
+    setLoading(true)
+    fetch(` http://localhost:8080/users/addCompleteKata?userId=${user.id}&kataId=${kata.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("ELearningToken")}`
       },
-     
+
     })
-    .then(res => res.json())
-    .then(data => {
-      getCompletedStuff(user.id, setCompletedLessons, setCompletedWeeks, setCompletedModules,setCompletedKatas)
-      //console.log(data);
-      
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .then(res => res.json())
+      .then(data => {
+        getCompletedStuff(user.id, setCompletedLessons, setCompletedWeeks, setCompletedModules, setCompletedKatas)
+        //console.log(data);
+        setIsCompleted(true);
+        setLoading(false);
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
 
   }
 
   const deleteKata = (e) => {
     e.stopPropagation();
-    
+
 
     fetch(`http://localhost:8080/katas/${kata.id}`, {
       method: "DELETE",
@@ -55,18 +61,18 @@ export default function NewKataCard({ kata }) {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("ELearningToken")}`,
       },
-     
+
     }).then(res => res.json())
-    .then(data => {
-      kataCardRef.current.remove();
-    })
-      e.target.disable()
+      .then(data => {
+        kataCardRef.current.remove();
+      })
+
   };
 
 
   const EditPen = () => {
     return (
-     user && user.role == "ADMIN" ?
+      user && user.role == "ADMIN" ?
         <Menu>
           <MenuHandler>
             <i className="fa-solid fa-pen p-2 text-first bg-[rgba(167,166,166,0.7)] hover:bg-[rgba(255,255,255,1)] rounded-xl duration-300 cursor-pointer mx-1 " ></i>
@@ -96,48 +102,39 @@ export default function NewKataCard({ kata }) {
 
   let maxPoints = 48;
   return (
-    <div id="cardHolder" className=" w-64 h-fit bg-[#fceeca] flex flex-col items-center border-2 border-[#aa6b48] rounded-2xl " ref={kataCardRef} >
-     
-        <p id="title" className="text-[#0b0f1b] mt-2 font-ninja">{kata.title}</p>
-        
-      
-
-      <div id="theme" className="w-48 h-32 mt-2 rounded-xl flex items-center justify-center  border-2 shadow-md border-[#aa6b48]" style={{ backgroundImage: `url('https://img.freepik.com/premium-photo/surprising-luxurious-background-design-with-golden-lotus-lotus-flowers-line-art-design-wallpaper-generative-ai_779468-4131.jpg')`, backgroundPosition: 'center' }}>
-
+    <div id="cardHolder" className=" w-64 h-fit bg-[#fceeca] flex flex-col items-center border-2 border-[#aa6b48] rounded-2xl relative " ref={kataCardRef} >
+     {loading && 
+      <div id="loadingContainer" className="absolute w-full h-full bg-black bg-opacity-70  items-center justify-center flex  border-2 rounded-2xl">
+      <Loading /> 
       </div>
-
-
-      
-
+      }
+      <p id="title" className="text-[#0b0f1b] mt-2 font-ninja">{kata.title}</p>
+      <div id="theme" className="w-48 h-32 mt-2 rounded-xl flex items-center justify-center  border-2 shadow-md border-[#aa6b48]" style={{ backgroundImage: `url('https://img.freepik.com/premium-photo/surprising-luxurious-background-design-with-golden-lotus-lotus-flowers-line-art-design-wallpaper-generative-ai_779468-4131.jpg')`, backgroundPosition: 'center' }}></div>
       <p className="text-[#0b0f1b] text-start relative flex items my-2">Training details</p>
+
       <div id="Details" className="text-[#0b0f1b] flex !flex-row">
         <div id="leftDetails" className="mr-1 border-2 rounded-xl border-[#aa6b48] min-w-32">
-          {/* <p id="tags" className="ml-1">Fndamentals</p>
-                    <p id="tags" className="ml-1">Mathematics</p>
-                    <p id="tags" className="ml-1">Algorithm</p> */}
-
           {kata.category?.map((category, index) => (
             <p id="tags" key={index} className="ml-1">{category}</p>
           ))}
-
         </div>
+
         <div id="rightDetails" className=" border-2 rounded-xl border-[#aa6b48] min-w-24">
-          <p id="pointsPerCompetion" className="ml-1 mt-1">Kyu: { kata.level}</p>
+          <p id="pointsPerCompetion" className="ml-1 mt-1">Kyu: {kata.level}</p>
           <p id="status" className="ml-1">Status</p>
         </div>
-
-
       </div>
+
       <div id="buttonsContainer" className="flex flex-row justify-evenly w-full mt-4 mb-4">
-        
-      {!isCompleted ?
-      (<button id="markedAsDone" className="text-[#0b0f1b] border border-[#aa6b48] rounded-full w-2/5 font-ninja left-0"
-        onClick={completeKataEvent}
-        >Done</button>) :
-        (
-        <button id="markedAsDone" className="text-[#0b0f1b] border border-[#aa6b48] rounded-full w-2/5 font-ninja left-0 cursor-default"
-        >Completed</button>
-        )}
+
+        {!isCompleted ?
+          (<button id="markedAsDone" className="text-[#0b0f1b] border border-[#aa6b48] rounded-full w-2/5 font-ninja left-0"
+            onClick={completeKataEvent}
+          >Done</button>) :
+          (
+            <button id="markedAsDone" className="text-[#0b0f1b] border border-[#aa6b48] rounded-full w-2/5 font-ninja left-0 cursor-default"
+            >Completed</button>
+          )}
 
         <a id="beginTraining" className="text-[#0b0f1b]  border border-[#aa6b48] rounded-full w-24 font-ninja flex items-center justify-center" href={kata.kataLink} target="_blank">Train</a>
         <EditPen />

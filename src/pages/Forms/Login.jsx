e659cd7e2eb56362881aa0a0240e3e3f073ba9e1
IacobOliver <olialexander08@gmail.com>
2024-04-05 15:47:03 +0300

    refactor + add hamburger header + modified login

--- src/pages/Forms/Login.jsx
+++ src/pages/Forms/Login.jsx
@@ -5,7 +5,7 @@ import { startLink } from "../../constants/Constants";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("Oli");
+  const [email, setEmail] = useState("oli@");
   const [password, setPassword] = useState("1234");
   const [error, setError] = useState(null);
 
@@ -13,8 +13,8 @@ const Login = () => {
     e.preventDefault();
     e.target.disabled = true
 
-    if (!username || !password) {
-      setError("Username and password are required");
+    if (!email || !password) {
+      setError("Email and password are required");
       return;
     }
 
@@ -27,7 +27,7 @@ const Login = () => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-             username : username.trim() ,
+             email : email.trim() ,
              password 
             }),
         }
@@ -35,8 +35,8 @@ const Login = () => {
 
       if (!response.ok) {
         console.log(response);
-        setError("Incorrect username or password");
-        setUsername("");
+        setError("Incorrect email or password");
+        setEmail("");
         setPassword("");
         e.target.disabled = false
       } else {
@@ -72,12 +72,12 @@ const Login = () => {
           <div className="relative h-11 w-full min-w-[200px]">
             <input
               type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
             />
             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-              Username
+              Email
             </label>
           </div>
           <div className="relative h-11 w-full min-w-[200px]">

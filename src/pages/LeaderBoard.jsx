2300232db852b9a35f865c908dc1462397fd5d07
IacobOliver <olialexander08@gmail.com>
2024-05-14 23:02:41 +0300

    leader board fix buttons

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -12,8 +12,8 @@ export default function LeaderBoard() {
     const [isWeekly, setIsWeekly] = useState(false);
 
     useEffect(() => {
-        fetchLeaderboardData(); 
-    }, []); 
+        fetchLeaderboardData();
+    }, []);
 
     const fetchLeaderboardData = (timePeriod = 'all') => {
         setLoading(true);
@@ -26,17 +26,17 @@ export default function LeaderBoard() {
                 Authorization: `Bearer ${localStorage.getItem("ELearningToken")}`
             },
         })
-        .then(res => res.json())
-        .then(data => {
-           
-            setPeople(data);
-            setIsWeekly(timePeriod === 'weekly'); 
-            setLoading(false);
-        })
-        .catch(err => {
-            console.error("Error fetching leaderboard:", err);
-            setLoading(false);
-        });
+            .then(res => res.json())
+            .then(data => {
+
+                setPeople(data);
+                setIsWeekly(timePeriod === 'weekly');
+                setLoading(false);
+            })
+            .catch(err => {
+                console.error("Error fetching leaderboard:", err);
+                setLoading(false);
+            });
     };
 
     const handleAllTimeButtonClick = () => {
@@ -51,13 +51,21 @@ export default function LeaderBoard() {
         <div className="h-screen flex flex-row text-sixth overflow-x-hidden overflow-y-scroll relative custom-scrollbar bg-generalColors-dark-blue text-gray-100">
             <SideHeader />
             <div id="moduleAndKataHolder" className="px-7" style={{ minWidth: "calc(100vw - 5rem)" }}>
-                {user && 
+                {user &&
                     <div id='container' className='w-full px-9 h-full justify-center flex flex-col'>
                         <p id='title' className='text-5xl justify-center flex text-white mb-16'>Leaderboard</p>
                         <div id='buttonContainer' className='flex justify'>
-                        <button id='allTime' className='bg-[#4ec49d] text-[#fefefe] text-2xl px-9 rounded-full' onClick={handleAllTimeButtonClick}>All time</button>
-            <button id='thisWeek' className='text-white text-2xl px-9 rounded-full border-2 border-white ml-3' onClick={handleWeeklyButtonClick}>This Week</button>
-            </div>
+                            <button id='allTime'
+                                    className={`${isWeekly ? "border-2 border-white bg-generalColors-dark-blue" : "bg-generalColors-medium-green border-none"}  text-[#fefefe] text-2xl px-9 rounded-full`} 
+                                    onClick={handleAllTimeButtonClick}>
+                                        All time
+                            </button>
+                            <button id='thisWeek' 
+                                    className={` ${!isWeekly ? "border-2 border-white bg-generalColors-dark-blue" : "bg-generalColors-medium-green border-none"}text-white text-2xl px-9 rounded-full  ml-3`}
+                                    onClick={handleWeeklyButtonClick}>
+                                        This Week
+                            </button>
+                        </div>
                         <div id='leaderboardContainer' className='w-full h-2/3 rounded-3xl bg-[#ffffff] mt-3 border-2 px-2 overflow-y-scroll'>
                             <div id='titles' className='w-full h-12 mt-4 border-b-2 flex flex-row bg-white sticky top-0 z-20 '>
                                 <p className='text-[#244476] w-20 text-xl flex items-center justify-center  pl-14'>Rank</p>
@@ -71,16 +79,16 @@ export default function LeaderBoard() {
                                     <p className='text-[#244476] text-xl pr-10'>Honor</p>
                                 </div>
                             </div>
-                            {loading ? <p>Loading...</p> : 
-                people.map((person, index) => (
-                    <Person
-                        key={index}
-                        rank={index + 1}
-                        name={`${person.firstName} ${person.lastName}`}
-                        xp={isWeekly ? person.weeklyRankPoints : person.rankPoints} // Conditionally pass XP based on isWeekly
-                    />
-                ))
-            }
+                            {loading ? <p>Loading...</p> :
+                                people.map((person, index) => (
+                                    <Person
+                                        key={index}
+                                        rank={index + 1}
+                                        name={`${person.firstName} ${person.lastName}`}
+                                        xp={isWeekly ? person.weeklyRankPoints : person.rankPoints} // Conditionally pass XP based on isWeekly
+                                    />
+                                ))
+                            }
                         </div>
                     </div>
                 }

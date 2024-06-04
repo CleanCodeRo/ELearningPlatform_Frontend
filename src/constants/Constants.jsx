023a212704d12cc7fbedc886c8c1163800dc553a
Alexandru-Marian Buza <alexandrumarianbuza@gmail.com>
2024-06-04 23:44:30 +0300

    Update Constants.jsx

--- src/constants/Constants.jsx
+++ src/constants/Constants.jsx
@@ -1,2 +1,3 @@
-export let startLink = "http://localhost:8080" //localHost
+//export let startLink = "http://localhost:8080" //localHost
 //export let startLink = "https://cleancode-academy.com" //localHost
+export let startLink = "http://backend:8080" //brolake rancher
